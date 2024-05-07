718c3318560a9be39192aec56ead963aded8f9b7
dipu-37 <razibul1105nstu@gmail.com>
2024-05-07 18:07:54 +0600

    bank section style finish

--- js/login.js
+++ js/login.js
@@ -1,17 +1,27 @@
 
+// click submit button and get id and password 
+
 document.getElementById('button_submit').addEventListener('click',function(){
     // console.log('ok');
+
     const emailfield =document.getElementById('user_email')
     const email = emailfield.value;
    
-
     const passwordfield =document.getElementById('user_password')
     const passeord = passwordfield.value;
     console.log(email,passeord);
+
+
     if(email ==='dipu141@'  && passeord ==='secret'){
-        console.log('valid user')
+        console.log('valid user');
+        // javascript click on button go to another html page 
+        window.location.href = "bank.html";
     }
     else{
         alert('invalid user')
     }
+
+    
+    
+ 
 })
